9468185c570df1a0d2f091d68ab652e9c452c624
billy8874 <biliyy224432@gmail.com>
2023-12-13 18:34:04 +0800

    Finished using curvature to reduce the number of points

--- method/PFH.py
+++ method/PFH.py
@@ -45,10 +45,12 @@ class PFH(object):
         :returns: normal vector
         """
         print("\tCalculating surface normals. \n")
-        normals = []
+        N = pc.shape[0]
+        normals = np.zeros((N, 1, 3))
+        filtered_pc_list = []
         ind_of_neighbors = []
         # print("pc", pc.shape)
-        N = pc.shape[0]
+        
         for i in range(N):
             # Get the indices of neighbors, it is a list of tuples (dist, indx)
             indN = self.get_neighbors(pc[i], pc) #<- old code
@@ -57,23 +59,27 @@ class PFH(object):
             # SVD to get normal
             X = pc[indN[:,0].astype(int), :]
             X = X - np.mean(X, axis=0)
-            cov = (X.T @ X)/(len(indN[:,0]))
-            _, _, Vt = np.linalg.svd(cov)
+            y = X / np.sqrt(X.shape[0]-1)
+            _, S, Vt = np.linalg.svd(y)
+            
+            # Curvature to decide which points to use in histogram
+            K = S[0]/np.sum(S)
+            if K>0.55: 
+                filtered_pc_list.append(i)
+            
             normal = Vt[2,:]
             # Re-orient normal vectors
             if normal @ (-1.*(pc[i])).T < 0:
                 normal = -1.*normal
-            normals.append(normal)
+            normals[i, :, :] = normal
 
-        return np.asarray(normals), ind_of_neighbors
+        return normals, ind_of_neighbors, filtered_pc_list
     
-    def calcHistArray(self, pc, normal, indNeigh):
+    def calcHistArray(self, pc, normal, indNeigh, pc_list):
         print("\tCalculating histograms naive method \n")
-
         N = len(pc)
-
         histograms = np.zeros((N, self.bin**3))
-        for i in range(N):
+        for i in pc_list:
             N_features = sp.comb(self.num_neighbors + 1, 2)
             point_ids = np.append(indNeigh[i], [i])
             point_pairs = np.array(np.meshgrid(point_ids, point_ids)).reshape(-1, 2)
@@ -96,6 +102,7 @@ class PFH(object):
 
             hist, edges = self.calc_hist(features)
             histograms[i, :] = hist / (N_features)
+        
         return histograms
     
     def calc_thresholds(self):
@@ -108,10 +115,6 @@ class PFH(object):
         return np.array(thresholds)
 
     def cal_bin(self, si, fi):
-        # result = 0
-        # for i, s in enumerate(si):
-        #     if fi >= s:
-        #         result = i+1
         return np.searchsorted(si, fi)
 
     def calc_hist(self, feature):
@@ -122,22 +125,7 @@ class PFH(object):
             hist - array of length div^3, represents number of samples per bin
             bin_edges - range(0, 1, 2, ..., (div^3+1)) 
         """
-        # # preallocate array sizes, create bin_edges
-        # # only 3 feature
-        # hist, edges = np.zeros(self.bin**3), np.arange(0,self.bin**3+1)
-        
-        # # find the division thresholds for the histogram
-        # threshold = self.calc_thresholds()
-        # # Loop for every row in f from 0 to N
-        # for j in range(0, feature.shape[0]):
-        #     # calculate the bin index to increment
-        #     index = 0
-        #     for i in range(3):
-        #         index += self.cal_bin(threshold[i, :], feature[j, i]) * (self.bin**(i))
-        #     # Increment histogram at that index
-        #     hist[index] += 1
-        
-        # return hist, edges
+
         hist, edges = np.histogramdd(feature, bins=self.bin)
         return hist.flatten(), edges[0]
 
@@ -150,30 +138,15 @@ class PFH(object):
 
         """
         print("...Matching point clouds. \n")
-        num_s = ps.shape[0]
-        num_t = pt.shape[0]
-        
         print("...Processing source point cloud...\n")
-        norm_s, ind_nei_s = self.calc_normals(ps)
-        hist_s = self.calcHistArray(ps, norm_s, ind_nei_s)
+        norm_s, ind_nei_s, filtered_ps_list = self.calc_normals(ps)
+        hist_s = self.calcHistArray(ps, norm_s, ind_nei_s, filtered_ps_list)
         
         print("...Processing target point cloud...\n")
-        norm_t,ind_nei_t = self.calc_normals(pt)
-        hist_t = self.calcHistArray(pt, norm_t, ind_nei_t)
-        
-        # dist = []
-        # matchInd = []
-        # distances = []
-        # for i in range(num_s):
-        #     for j in range(num_t):
-        #         #appending the l2 norm and j
-        #         dist.append((np.linalg.norm(hist_s[i]-hist_t[j]),j))
-        #     dist.sort(key=lambda x:x[0]) #To sort by first element of the tuple
-        #     matchInd.append(dist[0][1])
-        #     distances.append(dist[0][0])
-        #     dist = []
-        
-        distances = np.linalg.norm(hist_s[:, np.newaxis] - hist_t, axis=2)
+        norm_t, ind_nei_t, filtered_pt_list = self.calc_normals(pt)
+        hist_t = self.calcHistArray(pt, norm_t, ind_nei_t, filtered_pt_list)
+
+        distances = np.linalg.norm(hist_s[filtered_ps_list, np.newaxis] - hist_t[filtered_pt_list], axis=2)
         matchInd = np.argmin(distances, axis=1)
         distances = np.min(distances, axis=1)
-        return matchInd, distances
+        return matchInd, distances, filtered_ps_list, filtered_pt_list
